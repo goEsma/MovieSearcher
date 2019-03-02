ae949f0c2a83f8b640a6656b8946b4857ef93749
Esma GO <esmagoktekin01@gmail.com>
2019-03-02 07:40:15 +0300

    Add the Xcode project.

---
+++ MovieSearcher/MovieSearcher/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MovieSearcher
+//
+//  Created by Esma on 3/2/19.
+//  Copyright © 2019 Esma. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+
+}
+
---
+++ MovieSearcher/MovieSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearcher
+//
+//  Created by Esma on 3/2/19.
+//  Copyright © 2019 Esma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
