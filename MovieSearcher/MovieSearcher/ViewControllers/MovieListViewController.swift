2ba4d85947726518a0a849faf6dca3c4140083da
Esma GO <esmagoktekin01@gmail.com>
2019-03-02 13:58:44 +0300

    Create MovieListViewController.

---
+++ MovieSearcher/MovieSearcher/ViewControllers/MovieListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListViewController.swift
+//  MovieSearcher
+//
+//  Created by Esma on 3/2/19.
+//  Copyright © 2019 Esma. All rights reserved.
+//
+
+import UIKit
+
+final class MovieListViewController: UIViewController {
+    
+}
