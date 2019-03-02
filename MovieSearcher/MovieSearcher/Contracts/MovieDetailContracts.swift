22cdfc0bc650c2bcb60d6f174427533dc522d1fb
Esma GO <esmagoktekin01@gmail.com>
2019-03-02 22:34:13 +0300

    Add moviedetail contracts and presentation.

---
+++ MovieSearcher/MovieSearcher/Contracts/MovieDetailContracts.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailContracts.swift
+//  MovieSearcher
+//
+//  Created by Esma on 3/2/19.
+//  Copyright © 2019 Esma. All rights reserved.
+//
+
+import Foundation
+
+protocol MovieDetailViewProtocol {
+    func updateMovieDetail(_ movieDetail: MovieDetailPresentation)
+}
