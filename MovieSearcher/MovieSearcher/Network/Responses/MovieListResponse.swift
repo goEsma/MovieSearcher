a0e3bd0e97e9543673b20c35d0f6fb10e6262bd3
Esma GO <esmagoktekin01@gmail.com>
2019-03-03 12:37:13 +0300

    Get the total results from the movie list response.

--- MovieSearcher/MovieSearcher/Network/Responses/MovieListResponse.swift
+++ MovieSearcher/MovieSearcher/Network/Responses/MovieListResponse.swift
@@ -12,17 +12,21 @@ import Foundation
 struct MovieListResponse: Decodable {
     private enum RootCodingKeys: String, CodingKey {
         case search = "Search"
+        case totalResults
     }
 
     let movies: [Movie]
+    let totalResult:String
     
-    init(movies: [Movie]) {
+    init(movies: [Movie], totalResult:String) {
         self.movies = movies
+        self.totalResult = totalResult
     }
     
     public init(from decoder: Decoder) throws {
         let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
         self.movies = try rootContainer.decode([Movie].self, forKey: .search)
+        self.totalResult = try rootContainer.decode(String.self, forKey: .totalResults)
     }
     
 }
