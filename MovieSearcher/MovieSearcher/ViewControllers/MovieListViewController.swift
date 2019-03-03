d5e07779d9f957fd2491d3e101f32d17d8b934fe
Esma GO <esmagoktekin01@gmail.com>
2019-03-03 10:15:05 +0300

    Add filtering. [WIP]

--- MovieSearcher/MovieSearcher/ViewControllers/MovieListViewController.swift
+++ MovieSearcher/MovieSearcher/ViewControllers/MovieListViewController.swift
@@ -13,10 +13,12 @@ final class MovieListViewController: UIViewController {
     @IBOutlet weak var titleSearchBar: UISearchBar!
     @IBOutlet weak var yearSearchBar: UISearchBar!
     @IBOutlet weak var typeSearchBar: UISearchBar!
+    @IBOutlet weak var typePicker: UIPickerView!
     
     @IBOutlet private weak var tableView: UITableView!
     
-    
+    private var pickerList = ["movie", "episode", "series"]
+
     var viewModel: MovieListViewModelProtocol! {
         didSet {
             viewModel.delegate = self
@@ -29,6 +31,22 @@ final class MovieListViewController: UIViewController {
         super.viewDidLoad()
         self.tableView.register((UINib(nibName: "MovieTableViewCell", bundle: nil)), forCellReuseIdentifier: "MovieTableViewCell")
 
+        let searchButton = UIBarButtonItem(title: "Search", style: .plain, target: self, action: #selector(searchButtonTapped))
+        navigationItem.rightBarButtonItem = searchButton
+
+    }
+    
+    @objc func searchButtonTapped() {
+        guard let title = titleSearchBar.text, title != "" else {
+            print("error must enter title")
+            // Show an alert message.
+            return
+        }
+        let type = typeSearchBar.text
+        let year = yearSearchBar.text
+        viewModel.loadMovies(for: title, year: year, type: type)
+        self.resignFirstResponder()
+        self.view.endEditing(true)
     }
 
 }
@@ -69,7 +87,6 @@ extension MovieListViewController: UITableViewDataSource {
 }
 
 extension MovieListViewController: UITableViewDelegate {
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
         viewModel.selectMovie(at: indexPath.row)
@@ -80,26 +97,52 @@ extension MovieListViewController: UITableViewDelegate {
 //MARK: - UISearchBarDelegate
 extension MovieListViewController: UISearchBarDelegate {
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        searchBar.becomeFirstResponder()
+        if searchBar == typeSearchBar {
+            self.typePicker.isHidden = false
+            searchBar.resignFirstResponder()
+        }else {
+            searchBar.becomeFirstResponder()
+        }
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.frame = CGRect(x: 0.0, y: -80.0, width: 0.0, height: 44.0)
         searchBar.resignFirstResponder()
+        self.view.endEditing(true)
         searchBar.text = ""
+        if searchBar == typeSearchBar {
+            self.typePicker.isHidden = true
+        }
         
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let title = titleSearchBar.text, title != "" else {
-            print("error must enter title")
-            // Show an alert message. 
-            return
-        }
-        let type = typeSearchBar.text
-        let year = yearSearchBar.text
-        viewModel.loadMovies(for: title, year: year, type: type)
-        searchBar.resignFirstResponder()
+        searchButtonTapped()
+        self.view.endEditing(true)
     }
     
+}
+
+extension MovieListViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerList.count
+    }
+    
+    
+}
+
+extension MovieListViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.view.endEditing(true)
+        return pickerList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.typeSearchBar.text = self.pickerList[row]
+        self.typePicker.isHidden = true
+    }
 }
